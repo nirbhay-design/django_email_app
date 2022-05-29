dc53e70f3fb67cce7caa0924f3ce88788d0d43cb
sharma.59 <sharma.59@iitjac.in>
2022-05-29 23:03:29 +0530

    adding email project

--- email_system/settings.py
+++ email_system/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z=pfgsqg^8ws#cv3&kpc+0#hmn3oqs0-(zdz*#)qtn@@276o7f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['email-app-sharma59.herokuapp.com','127.0.0.1']
 
 
 # Application definition
